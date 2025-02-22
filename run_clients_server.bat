e87afa006b8bf53e30efb5d1c186e84eb1d92049
Shiny3 <z7.dimitrova@gmail.com>
2025-02-22 10:39:39 +0200

    Create Bat file to run directly from the Exe - Paralel Clients.
    TODO: Docker with Kubernetes - if have PC memory and time.

---
+++ run_clients_server.bat
@@ -0,0 +1,31 @@
+@echo off
+
+ 
+
+set ROOT_DIR=C:\Users\Shiny#\OneDrive\Documents\GitHub\TCP-Chat-OC\TCP-Chat-OC
+
+ 
+set client_exe=Client-OC\x64\Debug\Client-OC.exe
+set server_exe=Server-OC\x64\Release\Server-OC.exe
+
+set CLIENT_FULL_PATH=%ROOT_DIR%\%client_exe%
+set SERVER_FULL_PATH=%ROOT_DIR%\%server_exe%
+
+echo  "%CLIENT_FULL_PATH%" shown successfully.
+
+start "" "%CLIENT_FULL_PATH%"  # Run the first client instance
+REM start "" "%CLIENT_FULL_PATH%"  # Run the second client instance
+REM start "" "%CLIENT_FULL_PATH%"   # Run the third client instance
+
+ 
+
+REM This batch file starts a new command prompt window with parameters
+
+set host=127.0.0.1
+set port=12345
+
+REM Replace "yourCommand" with the command you want to run
+start "" "%CLIENT_FULL_PATH%" "%host%" "%port%"
+
+echo Client started on %host%:%port%.
+pause
